90afbcab7c82dbd1ec19062382281215f50b0c0c
majkusi <igor34p@gmail.com>
2026-03-24 14:18:32 +0100

    Added logs entry entity

---
+++ LogAnalyzer.Core/Entities/LogEntryAggregate/LogEntry.cs
@@ -0,0 +1,16 @@
+﻿using LogAnalyzer.Core.Common;
+using LogAnalyzer.Core.Entities.Enums;
+
+namespace LogAnalyzer.Core.Entities.LogEntryAggregate
+
+{
+    public class LogEntry : AggregateRoot
+    {
+        public DateTime TimeStamp { get; set; }
+        public LogLevelEnum LogLevel { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string? StackTrace { get; set; }
+        public string? Source { get; set; }
+
+    }
+}
