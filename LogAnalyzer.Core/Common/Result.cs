36158cadefb30084b342cd4630a4ef16dee09fff
majkusi <igor34p@gmail.com>
2026-03-24 15:03:13 +0100

    Added Result class for Result Pattern

---
+++ LogAnalyzer.Core/Common/Result.cs
@@ -0,0 +1,21 @@
+﻿namespace LogAnalyzer.Core.Common
+{
+    public sealed class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public T? Value { get; }
+        public string? Error { get; }
+        private Result(T value)
+        {
+            IsSuccess = true;
+            Value = value;
+        }
+        private Result(string error)
+        {
+            IsSuccess = false;
+            Error = error;
+        }
+        public static Result<T> Success(T value) => new Result<T>(value);
+        public static Result<T> Failure(string error) => new Result<T>(error);
+    }
+}
