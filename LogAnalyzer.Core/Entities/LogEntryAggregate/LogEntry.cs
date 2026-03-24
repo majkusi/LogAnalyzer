e1b56387e0a9b9ccf160fe016922209959357743
majkusi <igor34p@gmail.com>
2026-03-24 14:58:42 +0100

    Created constructor and added LogMessage value obj.

--- LogAnalyzer.Core/Entities/LogEntryAggregate/LogEntry.cs
+++ LogAnalyzer.Core/Entities/LogEntryAggregate/LogEntry.cs
@@ -4,13 +4,23 @@ using LogAnalyzer.Core.Entities.Enums;
 namespace LogAnalyzer.Core.Entities.LogEntryAggregate
 
 {
-    public class LogEntry : AggregateRoot
+    public sealed class LogEntry : AggregateRoot
     {
         public DateTime TimeStamp { get; set; }
         public LogLevelEnum LogLevel { get; set; }
-        public string Message { get; set; } = string.Empty;
+        public LogMessage? Message { get; set; }
         public string? StackTrace { get; set; }
         public string? Source { get; set; }
 
+        public LogEntry() { }
+        public LogEntry(DateTime timeStamp, LogLevelEnum logLevel, LogMessage message, string? stackTrace, string? source)
+        {
+            TimeStamp = timeStamp;
+            LogLevel = logLevel;
+            Message = message;
+            StackTrace = stackTrace;
+            Source = source;
+
+        }
     }
 }
---
+++ LogAnalyzer.Core/Entities/LogEntryAggregate/LogMessage.cs
@@ -0,0 +1,35 @@
+﻿using LogAnalyzer.Core.Common;
+
+namespace LogAnalyzer.Core.Entities.LogEntryAggregate
+{
+    public sealed class LogMessage : ValueObject
+    {
+        private const int MESSAGE_SANITY_LIMIT = 1000;
+
+        public string Message { get; private set; }
+
+        private LogMessage(string message)
+        {
+            Message = message;
+        }
+
+        public static Result<LogMessage> Create(string message)
+        {
+            if (String.IsNullOrEmpty(message))
+            {
+                return Result<LogMessage>.Failure("message is empty");
+            }
+            if (message.Length > MESSAGE_SANITY_LIMIT)
+            {
+                return Result<LogMessage>.Failure("message is too long");
+            }
+
+            return Result<LogMessage>.Success(new LogMessage(message));
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Message;
+        }
+    }
+}
