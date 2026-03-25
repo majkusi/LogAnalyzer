0be6b0fb366d1f0798f6d7288e3569cbf1323a6a
majkusi <igor34p@gmail.com>
2026-03-25 10:47:22 +0100

    Created basic events and changed Create methods

---
+++ LogAnalyzer.Core/Events/LogAddedToLogGroupEvent.cs
@@ -0,0 +1,8 @@
+﻿using LogAnalyzer.Core.Common;
+
+namespace LogAnalyzer.Core.Events
+{
+    public sealed record LogAddedToLogGroupEvent(Guid logId, Guid logGroupId) : IDomainEvent
+    {
+    }
+}
