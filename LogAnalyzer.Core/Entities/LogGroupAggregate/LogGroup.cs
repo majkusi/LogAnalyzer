83c77fb4b4195ef81d9a0beb8b3a50059d0522d9
majkusi <igor34p@gmail.com>
2026-03-25 08:09:32 +0100

    Created LogGroup aggregate with basic methods, moved Message VO to Common folder.

---
+++ LogAnalyzer.Core/Entities/LogGroupAggregate/LogGroup.cs
@@ -0,0 +1,35 @@
+﻿using LogAnalyzer.Core.Common;
+using LogAnalyzer.Core.Entities.LogEntryAggregate;
+
+namespace LogAnalyzer.Core.Entities.LogGroupAggregate
+{
+    public sealed class LogGroup : AggregateRoot
+    {
+        public LogMessage? message { get; set; }
+        public int count { get; set; }
+        public DateTime firstOccurence { get; set; }
+        public DateTime lastOccurence { get; set; }
+        public IEnumerable<LogEntry> logs { get; set; } = Enumerable.Empty<LogEntry>();
+
+        public LogGroup() { }
+        public LogGroup(LogMessage? message, int count, DateTime firstOccurence, DateTime lastOccurence, IEnumerable<LogEntry> logs)
+        {
+            this.message = message;
+            this.count = count;
+            this.firstOccurence = firstOccurence;
+            this.lastOccurence = lastOccurence;
+            this.logs = logs;
+        }
+
+        public void AddLog(LogEntry log)
+        {
+            if (log is null)
+                throw new ArgumentNullException(nameof(log));
+            if (log.Message != message)
+                throw new ArgumentException("Log message does not match LogGroup message!");
+            lastOccurence = DateTime.Now;
+            count++;
+            logs.Append(log);
+        }
+    }
+}
